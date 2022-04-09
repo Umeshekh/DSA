f285618b3dfe1d25e6e089b69c0fb81ef62cb16e
umesh ekhande <75421391+Umeshekh@users.noreply.github.com>
2022-04-09 11:41:55 +0530

    Update reverse linked list.java

--- LinkedList/reverse linked list.java
+++ LinkedList/reverse linked list.java
@@ -1,12 +1,9 @@
+package LinkedList;
 
-class Node {
-    int data;
-    Node next;
-    Node(int key){
-        data=key;
-        next=null;
-    }
-}
+import java.util.Scanner;
+
+
+
 
 public class LinkedList_2 {
 
@@ -23,16 +20,22 @@ public class LinkedList_2 {
         System.out.println("Enter the number of test cases");
         int t=sc.nextInt();
         while (t-- > 0) {
+            System.out.println("Enter total no of elements:");
             int n=sc.nextInt();
             int a=sc.nextInt();
             Node head=new Node(a);
             Node tail=head;
             for( int i=1;i<n;i++) {
+
                 tail.next=new Node(sc.nextInt());
                 tail=tail.next;
             }
+
             Solution1 ob=new Solution1();
+            System.out.println("Before reversing..");
+            printList(head);
             head= ob.reverse(head);
+            System.out.println("After reversing...");
             printList(head);
 
 
@@ -59,3 +62,21 @@ class Solution1{
     }
 
 }
+
+
+/*
+Enter the number of test cases
+1
+Enter total no of elements:
+5
+1
+2
+3
+4
+5
+Before reversing..
+1 2 3 4 5
+After reversing...
+5 4 3 2 1
+
+ */
