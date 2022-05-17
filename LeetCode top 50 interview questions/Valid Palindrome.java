cbbfde15b77ae84832fddb9a825ee305c6220d2b
umesh ekhande <75421391+Umeshekh@users.noreply.github.com>
2022-05-17 12:36:15 +0530

    Create Valid Palindrome.java

---
+++ LeetCode top 50 interview questions/Valid Palindrome.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.toLowerCase();
+      /* This takes advantage of the negation (^) operator in character classes 
+      which basically says, "match everything except the following characters."
+         */
+        s=s.replaceAll("[^a-z0-9]","");
+        int i=0,j=s.length()-1;
+        while(i<j) {
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        
+        return true;
+        
+    }
+}
