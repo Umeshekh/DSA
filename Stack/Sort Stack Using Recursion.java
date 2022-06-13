57281fd0ef1c00a7f137bfd97f1a58a4d056098a
umesh ekhande <75421391+Umeshekh@users.noreply.github.com>
2022-06-13 20:31:14 +0530

    Create Sort Stack Using Recursion.java

---
+++ Stack/Sort Stack Using Recursion.java
@@ -0,0 +1,35 @@
+class GfG{
+	public Stack<Integer> sort(Stack<Integer> s)
+	{
+	    
+	    if(s.size()==1 ) {
+	        return s;
+	    }
+	    int max = Integer.MIN_VALUE;
+	    Stack<Integer> st = new Stack<>();
+	    while(!s.empty()) {
+	        
+	        if(s.peek()>max) {
+	            if(max==Integer.MIN_VALUE) {
+	                max=s.peek();
+	            }
+	            else {
+	                st.push(max);
+	                max=s.peek();
+	            }
+	            s.pop();
+	        }
+	        else {
+	            st.push(s.pop());
+	        }
+	        
+	        
+	    }
+	    Stack<Integer> ans = sort(st);
+	    ans.push(max);
+	    return ans;
+	    
+	    
+	}
+
+}
